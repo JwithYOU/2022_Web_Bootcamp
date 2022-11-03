24b1cb0d88fdeaeccd126c815d2f6a603c7efa77
YOU <khy960101@gamil.com>
2022-11-03 16:44:07 +0900

    signup 페이지 적묭 및 body-parser 사용

---
+++ Section20_API/Newsletter-Signup/app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+
+const app = express();
+
+
+// 서버가 CSS 및 정적파일을 제공하려면 express.stactic을 활용해서 사용할 수 있다.
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get("/", function(req, res){
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/",function(req, res){
+  const fName = req.body.firstName;
+  const lName = req.body.lastName;
+  const email = req.body.email;
+
+  console.log(fName, lName, email);
+});
+
+app.listen(3000, function(){
+  console.log("Server is running on port 3000");
+});
