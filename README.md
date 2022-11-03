66c6936bf6e21ef03811af80ff73c1ce74a9cd0e
YOU <khy960101@gmail.com>
2022-11-03 18:04:10 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# The Complete 2022 Web Development Bootcamp
+Udemy 2022 web bootcamp 강의 section 모음
+
--- Section20_API/Newsletter-Signup/app.js
+++ Section20_API/Newsletter-Signup/app.js
@@ -4,9 +4,12 @@ const request = require("request");
 
 const app = express();
 
+// 업데이트 여부
 
 // 서버가 CSS 및 정적파일을 제공하려면 express.stactic을 활용해서 사용할 수 있다.
 app.use(express.static("public"));
+
+// html에서 post로 넘겨준 데이터를 받기위해서는 body-parser가 필요하다.
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function(req, res){
